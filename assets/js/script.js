e2fb977d46b3a3ff9eec81a241b84b95b8aef1e8
mostarymithi <mostarymithi@gmail.com>
2019-02-01 23:53:49 +0600

    this is for practice

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// <!-- arrow js -->
+
+    
+        $(function () {
+            $('#scrollToBottom').bind("click", function () {
+                $('html, body').animate({ scrollTop: $(document).height() }, 1200);
+                return false;
+            });
+            $('#scrollToTop').bind("click", function () {
+                $('html, body').animate({ scrollTop: 0 }, 1200);
+                return false;
+            });
+        });
+  
+// <!-- scroll bottom js end -->
+
+
+
